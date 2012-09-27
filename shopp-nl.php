a04f330cb011c1ffe1f240768663377ed5cea884
Remco Tolsma <info@remcotolsma.nl>
2012-09-27 15:08:52 +0200

    Improved translations for Shopp v.1.2.3

--- shopp-nl.php
+++ shopp-nl.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Shopp (nl)
 Plugin URI: http://pronamic.eu/wp-plugins/shopp-nl/
-Description: Extends the Shopp plugin and add-ons with the Dutch language: <strong>Shopp</strong> 1.2.2
+Description: Extends the Shopp plugin and add-ons with the Dutch language: <strong>Shopp</strong> 1.2.3
 
-Version: 0.1
+Version: 0.1.1
 Requires at least: 3.0
 
 Author: Pronamic
